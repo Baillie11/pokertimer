943f5e32faf26e58af154502f948af907d8a4d2f
Andrew Baillie <andrewgbaillie@hotmail.com>
2021-05-06 16:47:44 +1000

    first save

--- main.js
+++ main.js
@@ -1,37 +1,43 @@
-let timeseconds=1800;
+let timeseconds=10;
 const timer = document.getElementById('countdown_clock');
-
-//timer.innerHTML = "00:" + timeseconds;
-
-const CountDown = setInterval (()=>{
-     timeseconds--;
-     DisplayTime(timeseconds);
-     if(timeseconds <=0 || timeseconds<1){
-         clearInterval(CountDown)
-     }  
-    },1000)
-
-
-
-function DisplayTime(second) {
-    const min = Math.floor(second / 60);
-    const sec = Math.floor(second % 60);
-    timer.innerHTML = `
-    ${min < 10 ? "0" : ""}${min}:${sec < 10 ? "0" : ""}${sec}
-    `;
-}
+var CountDown;
 
     // Pause or continue the Countdown Clock
 
 let btn = document.getElementById("play"); 
 var clicked = false;
+
 function toggle() {
     if (clicked == false) {
         clicked=true
         btn.innerHTML="Pause"
+
+        clearInterval(CountDown);
+        
+        //countdown our timer
+        CountDown = setInterval (()=>{
+            timeseconds--;
+            DisplayTime(timeseconds);
+            if(timeseconds <=0 || timeseconds<1){
+                clearInterval(CountDown);
+            }  
+            
+        },1000);
+
+        function DisplayTime(second) {
+            const min = Math.floor(second / 60);
+            const sec = Math.floor(second % 60);
+
+            timer.innerHTML = `
+            ${min < 10 ? "0" : ""}${min}:${sec < 10 ? "0" : ""}${sec}
+            `;
+        };
     } else {
-        clicked=false
-        btn.innerHTML="Play"
+        clicked=false;
+        btn.innerHTML="Play";
+        if(timeseconds <=0 || timeseconds<1){
+            clearInterval(CountDown);
+        }  
     }
 }
 
