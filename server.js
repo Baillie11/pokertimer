7c72c4003e2cfa0ef6fd796d94d27871ae734ee6
Andrew Baillie <andrewgbaillie@hotmail.com>
2021-09-19 14:00:33 +1000

    rendering html

--- server.js
+++ server.js
@@ -1,24 +1,44 @@
 const http = require('http');
 const fs = require('fs');
+const xlsx = require('xlsx');
+const jsontoxml = require('jsontoxml');
+const express = require('express');
+const app = express();
 
+//read XL file
+//try {
+const workbook = xlsx.readFile('blinds.xlsx');
+//    console.log(data);
+//} catch (err) {
+//    console.error(err);
+//}
 
-try {
-    const data = fs.readFileSync('hi.txt', 'utf8');
-    console.log(data);
-} catch (err) {
-    console.error(err);
+//Convert XLSX to JSON
+const worksheets = {};
+for (const sheetName of workbook.SheetNames) {
+    worksheets[sheetName] = xlsx.utils.sheet_to_json(workbook.Sheets[sheetName]);
+
+    //Show the data as JSON
+    //console.log('json:\n', JSON.stringify(worksheets.Sheet1), "\n\n");
 }
 
+module.exports = worksheets;
 
-const HOSTNAME = "localhost";
-const PORT = 3000;
+//static files
+app.use(express.static('/public'));
+app.use('/css', express.static(__dirname + '/public/css'));
+app.use('/js', express.static(__dirname + '/public/js'));
+app.use('/img', express.static(__dirname + 'img'));
 
-const server = http.createServer((req, res) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    res.end('Hello World');
-});
+//set views
+app.set('views',
 
-server.listen(PORT, HOSTNAME, () => {
-    console.log('Server runnung at http://localhost:3000/');
-});
+        app.get('', (req, res) => {
+            res.status(200);
+            res.render(__dirname + '/public/index.ejs')
+        });
+
+        app.listen(3000); console.log('listening on port 3000');
+
+
+        //console.log(worksheets);
